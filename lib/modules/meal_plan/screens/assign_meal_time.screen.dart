52dd267eb3795a1b0e59a7c8261366e1c4b92102
ecoknows <jerico.villaraza19@gmail.com>
2022-01-12 17:17:26 +0800

    :sparkles: feat: add hive box, fetch database feature, put or add feature to the box

--- lib/modules/meal_plan/screens/assign_meal_time.screen.dart
+++ lib/modules/meal_plan/screens/assign_meal_time.screen.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-import 'package:kubo/constants/colors.constants.dart';
 import 'package:kubo/constants/list.costants.dart';
 import 'package:kubo/constants/string.constants.dart';
 import 'package:kubo/constants/text_styles.constants.dart';
 import 'package:kubo/core/models/schedule.hive.dart';
 import 'package:kubo/modules/meal_plan/models/recipe.dart';
 import 'package:kubo/widgets/buttons/square.button.dart';
-import 'package:kubo/widgets/cards/picker.card.dart';
 import 'package:kubo/widgets/clippers/recipe.clipper.dart';
 import 'package:direct_select_flutter/direct_select_container.dart';
 import 'package:kubo/widgets/selectors/direct.selector.dart';
@@ -34,17 +32,27 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
   String? day = 'Monday';
   TimeOfDay? start;
   TimeOfDay? end;
+  Box<dynamic>? scheduleBox;
+  ScheduleHive? schedule;
 
-  Future checkSchedule() async {
-    var bahayKuboBox = await Hive.openBox(kBahayKuboBox);
-    // print(bahayKuboBox.getAt(0));
+  Future<void> initializeBox() async {
+    scheduleBox = await Hive.openBox(kScheduleBox);
+    if (scheduleBox != null) {
+      final args = ModalRoute.of(context)!.settings.arguments
+          as AssignMealTimeScreenArguments;
+
+      final Recipe recipe = args.recipe;
+
+      setState(() {
+        schedule = scheduleBox!.get(recipe.id);
+      });
+    }
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    checkSchedule();
+    initializeBox();
   }
 
   @override
@@ -122,6 +130,8 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
                     ),
                     DirectSelector(
                       list: kDayList,
+                      initialDay:
+                          schedule != null ? schedule!.scheduledDay : null,
                       leadingIcon: Icons.calendar_today,
                       onSelected: (String? daySelected) {
                         day = daySelected;
@@ -129,12 +139,18 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
                     ),
                     TimeSelector(
                       title: 'Start',
+                      initialTime: schedule != null
+                          ? stringToTimeOfDay(schedule!.startingTime)
+                          : null,
                       onTimePicked: (TimeOfDay? startTimePicked) {
                         start = startTimePicked;
                       },
                     ),
                     TimeSelector(
                       title: 'End',
+                      initialTime: schedule != null
+                          ? stringToTimeOfDay(schedule!.endingTime)
+                          : null,
                       onTimePicked: (TimeOfDay? endTimePicked) {
                         end = endTimePicked;
                       },
@@ -142,15 +158,17 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
                     SquareButton(
                       onPressed: () async {
                         if (day != null && start != null && end != null) {
-                          var bahayKuboBox = await Hive.openBox(kBahayKuboBox);
-                          var newSchedule = ScheduleHive();
+                          ScheduleHive newSchedule = ScheduleHive(
+                            recipeName: recipe.name,
+                            scheduledDay: day!,
+                            startingTime: start!.format(context),
+                            endingTime: end!.format(context),
+                          );
 
-                          newSchedule.recipeName = recipe.name;
-                          newSchedule.scheduledDay = day;
-                          newSchedule.startingTime = start;
-                          newSchedule.endingTime = end;
-
-                          bahayKuboBox.add(newSchedule);
+                          scheduleBox!.put(recipe.id, newSchedule);
+                          setState(() {
+                            schedule = newSchedule;
+                          });
                         }
                       },
                     )
@@ -180,11 +198,13 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
               left: 16,
               child: SizedBox(
                 width: size.width - 40,
-                child: const Text(
-                  'No schedule',
+                child: Text(
+                  schedule != null
+                      ? _scheduleFormatter(schedule)
+                      : 'No schedule',
                   overflow: TextOverflow.ellipsis,
                   maxLines: 1,
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 14.0,
                     fontWeight: FontWeight.w300,
@@ -195,6 +215,22 @@ class _AssignMealTimeScreenState extends State<AssignMealTimeScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  String _scheduleFormatter(ScheduleHive? scheduleHive) {
+    return '${scheduleHive!.scheduledDay}, ${scheduleHive.startingTime} - ${scheduleHive.endingTime}';
+  }
+
+  TimeOfDay stringToTimeOfDay(String time) {
+    int hh = 0;
+    if (time.endsWith('PM')) hh = 12;
+    time = time.split(' ')[0];
+    return TimeOfDay(
+      hour: hh +
+          int.parse(time.split(":")[0]) %
+              24, // in case of a bad time format entered manually by the user
+      minute: int.parse(time.split(":")[1]) % 60,
     );
   }
 }
