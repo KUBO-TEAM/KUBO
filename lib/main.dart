1f25025e7d6f6374989d329d0f0db8f8118c6f94
serix123 <jake.suerte@gmail.com>
2022-01-26 16:42:09 +0800

    added Hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ import 'package:kubo/modules/meal_plan/screens/create_meal_plan.screen.dart';
 import 'package:kubo/modules/menu/screens/menu.screen.dart';
 import 'package:kubo/modules/agenda/screens/agenda.screen.dart';
 import 'package:kubo/modules/recipe/screen/recipe.screen.dart';
+import 'package:kubo/modules/recipe/screen/recipeSteps.screen.dart';
 import 'package:kubo/modules/reminders/screen/reminders.screen.dart';
 import 'package:provider/provider.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
@@ -64,6 +65,7 @@ class Kubo extends StatelessWidget {
           AssignMealTimeScreen.id: (context) => const AssignMealTimeScreen(),
           RecipeScreen.id: (context) => const RecipeScreen(),
           ReminderScreen.id: (context) => const ReminderScreen(),
+          RecipeSteps.id:(context) => const RecipeSteps(),
         },
       ),
     );
--- lib/modules/recipe/screen/recipe.screen.dart
+++ lib/modules/recipe/screen/recipe.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:kubo/constants/colors.constants.dart';
 import 'package:kubo/constants/text_styles.constants.dart';
 import 'package:kubo/modules/meal_plan/recipes.examples.dart';
+import 'package:kubo/modules/recipe/screen/recipeSteps.screen.dart';
 import 'package:kubo/widgets/text_fields/search.field.dart';
 
 class RecipeScreen extends StatefulWidget {
@@ -52,53 +53,59 @@ class _RecipeScreenState extends State<RecipeScreen> {
                     height: 13.0,
                   ),
                   itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      elevation: 2,
-                      margin: EdgeInsets.zero,
-                      child: Container(
-                        height: 120,
-                        padding: const EdgeInsets.all(8),
-                        color: Colors.white,
-                        child: Row(
-                          children: [
-                            Card(
-                              semanticContainer: true,
-                              clipBehavior: Clip.antiAliasWithSaveLayer,
-                              shape: const RoundedRectangleBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(5.0)),
+                    return GestureDetector(
+                      onTap: () => Navigator.pushNamed(context, RecipeSteps.id,arguments: popularRecipes[index]),
+                      child: Card(
+                        elevation: 2,
+                        margin: EdgeInsets.zero,
+                        child: Container(
+                          height: 120,
+                          padding: const EdgeInsets.all(8),
+                          color: Colors.white,
+                          child: Row(
+                            children: [
+                              Card(
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(5.0)),
+                                ),
+                                child: Hero(
+                                  tag: "recipe-img-${popularRecipes[index].imageUrl}",
+                                  child: Image.network(
+                                    popularRecipes[index].imageUrl,
+                                    fit: BoxFit.cover,
+                                    height: 112.0,
+                                    width: 112.0,
+                                  ),
+                                ),
                               ),
-                              child: Image.network(
-                                popularRecipes[index].imageUrl,
-                                fit: BoxFit.cover,
-                                height: 112.0,
-                                width: 112.0,
-                              ),
-                            ),
-                            const SizedBox(width: 10.0),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    popularRecipes[index].name,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                    style: kSubTitleTextStyle.copyWith(
-                                      fontWeight: FontWeight.w900,
+                              const SizedBox(width: 10.0),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      popularRecipes[index].name,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                      style: kSubTitleTextStyle.copyWith(
+                                        fontWeight: FontWeight.w900,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    popularRecipes[index].description,
-                                    style: kCaptionTextStyle,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 2,
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
+                                    Text(
+                                      popularRecipes[index].description,
+                                      style: kCaptionTextStyle,
+                                      overflow: TextOverflow.ellipsis,
+                                      maxLines: 2,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
