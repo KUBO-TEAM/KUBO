16444de5c7f74ea8232af726837e2d3a8560cc87
ecoknows <jerico.villaraza19@gmail.com>
2022-02-06 15:49:56 +0800

    :sparkles: feat: add Meal Plan Cubit and Meal Plan State

--- lib/modules/meal_plan/bloc/meal_plan_state.dart
+++ lib/modules/meal_plan/bloc/meal_plan_state.dart
@@ -4,3 +4,13 @@ part of 'meal_plan_cubit.dart';
 abstract class MealPlanState {}
 
 class MealPlanInitial extends MealPlanState {}
+
+class CellDateSetSuccess extends MealPlanState {
+  final DateTime? startingDate;
+
+  CellDateSetSuccess({
+    required this.startingDate,
+  });
+}
+
+class CellDateRemoveSuccess extends MealPlanState {}
